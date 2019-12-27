c73ec40be3a4f058d882c9e9a2b837dccdaa8a35
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2019-12-27 16:44:30 +0900

    死亡した後で攻撃できてしまう点

--- src/main/jp/ac/uryukyu/ie/e195764/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195764/Enemy.java
@@ -46,9 +46,14 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        //修正箇所
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(dead) {
+            this.attack = 0;
+        } else {
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
--- src/main/jp/ac/uryukyu/ie/e195764/Main.java
+++ src/main/jp/ac/uryukyu/ie/e195764/Main.java
@@ -5,13 +5,13 @@ package jp.ac.uryukyu.ie.e195764;
  */
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
+        Hero hero = new Hero("勇者", 10, 5);//インスタンス生成→Heroコンストラクタ起動
+        Enemy enemy = new Enemy("スライム", 6, 3);//インスタンス生成→Enemyコンストラクタ起動
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( hero.isDead() == false && enemy.isDead() == false ){
+        while(!hero.isDead() && !enemy.isDead()){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
