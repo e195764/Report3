e93b831825d70d49116fdb74afc11137f28f2b42
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2019-12-30 21:37:09 +0900

    EnemyクラスとHeroクラスの重複

---
+++ src/main/jp/ac/uryukyu/ie/e195764/LivingThing.java
@@ -0,0 +1,60 @@
+package jp.ac.uryukyu.ie.e195764;
+
+/**
+ * 生物クラス。
+ *  String name; //生物の名前
+ *  int hitPoint; //生物のHP
+ *  int attack; //生物の攻撃力
+ *  boolean dead; //生物の生死状態。true=死亡。
+ */
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name 生物名
+     * @param maximumHP 生物のHP
+     * @param attack 生物の攻撃力
+     */
+    public LivingThing(String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(LivingThing opponent) {
+        int damage = (int)(Math.random() * attack);
+        if(dead) {
+            this.attack = 0;
+        } else {
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
