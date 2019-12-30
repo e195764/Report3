2031e2f9604dbb0ffb7500440f056d14f332a574
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2019-12-31 00:15:28 +0900

    カプセル化

--- src/main/jp/ac/uryukyu/ie/e195764/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195764/Enemy.java
@@ -14,12 +14,13 @@ public class Enemy extends LivingThing {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    int hp = getHitPoint();
     @Override
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+        hp -= damage;
+        if( hp < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 }
--- src/main/jp/ac/uryukyu/ie/e195764/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e195764/Hero.java
@@ -14,12 +14,13 @@ public class Hero extends LivingThing {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    int hp = getHitPoint();
     @Override
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        hp -= damage;
+        if( hp < 0 ) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
 }
